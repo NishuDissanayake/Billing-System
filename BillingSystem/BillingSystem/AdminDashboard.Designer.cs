232ad849baf33c8fea322a310bd0e9b2bfd76f02
NishuDissanayake <nishudissanayake5252@gmail.com>
2021-04-24 20:53:33 +0530

    Added footer

--- BillingSystem/BillingSystem/AdminDashboard.Designer.cs
+++ BillingSystem/BillingSystem/AdminDashboard.Designer.cs
@@ -29,20 +29,51 @@ namespace BillingSystem
         /// </summary>
         private void InitializeComponent()
         {
+            this.Footer = new System.Windows.Forms.Panel();
+            this.FooterLable = new System.Windows.Forms.Label();
+            this.Footer.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // Footer
+            // 
+            this.Footer.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.Footer.Controls.Add(this.FooterLable);
+            this.Footer.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Footer.Location = new System.Drawing.Point(0, 404);
+            this.Footer.Name = "Footer";
+            this.Footer.Size = new System.Drawing.Size(693, 20);
+            this.Footer.TabIndex = 0;
+            // 
+            // FooterLable
+            // 
+            this.FooterLable.AutoSize = true;
+            this.FooterLable.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FooterLable.Location = new System.Drawing.Point(500, 0);
+            this.FooterLable.Name = "FooterLable";
+            this.FooterLable.Size = new System.Drawing.Size(211, 17);
+            this.FooterLable.TabIndex = 0;
+            this.FooterLable.Text = "Developed By: Nishu Dissanayake";
+            // 
+            // AdminDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(571, 320);
-            this.Name = "Form1";
-            this.Text = "Billing System";
+            this.ClientSize = new System.Drawing.Size(693, 424);
+            this.Controls.Add(this.Footer);
+            this.Name = "AdminDashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Admin Dashboard";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Footer.ResumeLayout(false);
+            this.Footer.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel Footer;
+        private System.Windows.Forms.Label FooterLable;
     }
 }
 
