dd9b080c1984d435815d073e7a2c8164b08ab107
NishuDissanayake <nishudissanayake5252@gmail.com>
2021-04-25 22:55:08 +0530

    Basic menu

--- BillingSystem/BillingSystem/AdminDashboard.Designer.cs
+++ BillingSystem/BillingSystem/AdminDashboard.Designer.cs
@@ -31,7 +31,16 @@ namespace BillingSystem
         {
             this.Footer = new System.Windows.Forms.Panel();
             this.FooterLable = new System.Windows.Forms.Label();
+            this.TopMenu1 = new System.Windows.Forms.MenuStrip();
+            this.usersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.categoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.productsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inventoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.transactionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.UserLabel = new System.Windows.Forms.Label();
+            this.UserNameLabel = new System.Windows.Forms.Label();
             this.Footer.SuspendLayout();
+            this.TopMenu1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Footer
@@ -48,25 +57,96 @@ namespace BillingSystem
             // 
             this.FooterLable.AutoSize = true;
             this.FooterLable.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FooterLable.Location = new System.Drawing.Point(500, 0);
+            this.FooterLable.Location = new System.Drawing.Point(240, 0);
             this.FooterLable.Name = "FooterLable";
             this.FooterLable.Size = new System.Drawing.Size(211, 17);
             this.FooterLable.TabIndex = 0;
             this.FooterLable.Text = "Developed By: Nishu Dissanayake";
+            // 
+            // TopMenu1
+            // 
+            this.TopMenu1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usersToolStripMenuItem,
+            this.categoryToolStripMenuItem,
+            this.productsToolStripMenuItem,
+            this.inventoryToolStripMenuItem,
+            this.transactionsToolStripMenuItem});
+            this.TopMenu1.Location = new System.Drawing.Point(0, 0);
+            this.TopMenu1.Name = "TopMenu1";
+            this.TopMenu1.Size = new System.Drawing.Size(693, 24);
+            this.TopMenu1.TabIndex = 1;
+            this.TopMenu1.Text = "menuStrip1";
+            // 
+            // usersToolStripMenuItem
+            // 
+            this.usersToolStripMenuItem.Name = "usersToolStripMenuItem";
+            this.usersToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.usersToolStripMenuItem.Text = "Users";
+            // 
+            // categoryToolStripMenuItem
+            // 
+            this.categoryToolStripMenuItem.Name = "categoryToolStripMenuItem";
+            this.categoryToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.categoryToolStripMenuItem.Text = "Category";
+            // 
+            // productsToolStripMenuItem
+            // 
+            this.productsToolStripMenuItem.Name = "productsToolStripMenuItem";
+            this.productsToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.productsToolStripMenuItem.Text = "Products";
+            // 
+            // inventoryToolStripMenuItem
+            // 
+            this.inventoryToolStripMenuItem.Name = "inventoryToolStripMenuItem";
+            this.inventoryToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.inventoryToolStripMenuItem.Text = "Inventory";
+            // 
+            // transactionsToolStripMenuItem
+            // 
+            this.transactionsToolStripMenuItem.Name = "transactionsToolStripMenuItem";
+            this.transactionsToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.transactionsToolStripMenuItem.Text = "Transactions";
+            // 
+            // UserLabel
+            // 
+            this.UserLabel.AutoSize = true;
+            this.UserLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserLabel.Location = new System.Drawing.Point(12, 34);
+            this.UserLabel.Name = "UserLabel";
+            this.UserLabel.Size = new System.Drawing.Size(43, 16);
+            this.UserLabel.TabIndex = 2;
+            this.UserLabel.Text = "User: ";
+            // 
+            // UserNameLabel
+            // 
+            this.UserNameLabel.AutoSize = true;
+            this.UserNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserNameLabel.ForeColor = System.Drawing.Color.DarkBlue;
+            this.UserNameLabel.Location = new System.Drawing.Point(53, 34);
+            this.UserNameLabel.Name = "UserNameLabel";
+            this.UserNameLabel.Size = new System.Drawing.Size(142, 16);
+            this.UserNameLabel.TabIndex = 3;
+            this.UserNameLabel.Text = "Nishu Dissanayake";
             // 
             // AdminDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(693, 424);
+            this.Controls.Add(this.UserNameLabel);
+            this.Controls.Add(this.UserLabel);
             this.Controls.Add(this.Footer);
+            this.Controls.Add(this.TopMenu1);
+            this.MainMenuStrip = this.TopMenu1;
             this.Name = "AdminDashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Admin Dashboard";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Footer.ResumeLayout(false);
             this.Footer.PerformLayout();
+            this.TopMenu1.ResumeLayout(false);
+            this.TopMenu1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,6 +154,14 @@ namespace BillingSystem
 
         private System.Windows.Forms.Panel Footer;
         private System.Windows.Forms.Label FooterLable;
+        private System.Windows.Forms.MenuStrip TopMenu1;
+        private System.Windows.Forms.ToolStripMenuItem usersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem categoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem productsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inventoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem transactionsToolStripMenuItem;
+        private System.Windows.Forms.Label UserLabel;
+        private System.Windows.Forms.Label UserNameLabel;
     }
 }
 
