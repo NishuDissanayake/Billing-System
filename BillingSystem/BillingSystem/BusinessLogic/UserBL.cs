4399ee2e644adddc49e21bf78c0dadc0cf86e768
NishuDissanayake <nishudissanayake5252@gmail.com>
2021-06-09 21:08:41 +0530

    user getters and setters

---
+++ BillingSystem/BillingSystem/BusinessLogic/UserBL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.BusinessLogic
+{
+    class UserBL
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+
+        public string lastname { get; set; }
+
+        public string email { get; set; }
+
+        public string username { get; set; }
+
+        public string password { get; set; }
+
+        public string contact { get; set; }
+
+        public string gender { get; set; }
+
+        public string usertype { get; set; }
+
+        public string addeddate { get; set; }
+
+        public int addedby { get; set; }
+    }
+}
