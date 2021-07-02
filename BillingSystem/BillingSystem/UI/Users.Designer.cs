e2c758f1fa2c69da3369d3ec8012ef58bb7995d0
NishuDissanayake <nishudissanayake5252@gmail.com>
2021-07-03 00:23:30 +0530

    Data grid - users

--- BillingSystem/BillingSystem/UI/Users.Designer.cs
+++ BillingSystem/BillingSystem/UI/Users.Designer.cs
@@ -74,6 +74,7 @@ namespace BillingSystem.UI
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(800, 41);
             this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // CloseButton
             // 
